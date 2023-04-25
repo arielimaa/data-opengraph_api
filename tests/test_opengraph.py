4d1aaf85176cbe83a73dd421fb13a54d5d43a8e4
Ariele Amorim <ariele-plra@hotmail.com>
2023-04-25 12:11:18 +0200

    done

--- tests/test_opengraph.py
+++ tests/test_opengraph.py
@@ -6,7 +6,7 @@ from opengraph import fetch_metadata
 class TestOpenGraph(unittest.TestCase):
     def test_github_com(self):
         data = fetch_metadata("https://www.github.com")
-        self.assertRegex(data["title"], r'(?i)github')
+        self.assertRegex(data["data"]["title"], r'(?i)github')
 
     def test_wrong_url_returns_dict(self):
         data = fetch_metadata("https://www.a.com") # Does not exist
