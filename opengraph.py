632b2e44114587fe1759f06d6ec620b16662007e
Ariele Amorim <ariele-plra@hotmail.com>
2023-04-25 11:38:22 +0200

    done

--- opengraph.py
+++ opengraph.py
@@ -2,16 +2,24 @@
 """
 Client of the Wagon OpenGraph API
 """
-
+import pprint
 import requests
 
 def fetch_metadata(url):
     """
     Return a dictionary of OpenGraph metadata found in HTML of given url
     """
-    pass  # YOUR CODE HERE
+    response_dict= {}
+    params= f'url={url}'
+    api_url= 'https://opengraph.lewagon.com'
+    try:
+        response = requests.get(api_url, params= params)
+        response.raise_for_status()
+        return response.json()
+    except:
+        return response_dict
+
 
 # To manually test, please uncomment the following lines and run `python opengraph.py`:
-# import pprint
-# pp = pprint.PrettyPrinter(indent=4)
-# pp.pprint(fetch_metadata("https://www.github.com"))
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(fetch_metadata("https://www.github.com"))
